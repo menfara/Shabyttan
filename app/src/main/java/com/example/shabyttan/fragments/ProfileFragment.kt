ef15409f5218a4a43c079eac396df59ff13f348f
Farkhat <fmyrzabekov@gmail.com>
2023-09-23 14:33:27 +0600

    bottom navigation done

--- app/src/main/java/com/example/shabyttan/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/shabyttan/fragments/ProfileFragment.kt
@@ -1,21 +1,20 @@
 package com.example.shabyttan.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.shabyttan.R
 
 
-class HomeFragment : Fragment() {
+class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
-
 
 }
